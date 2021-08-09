fe6e076439104aecc2f5f4b1e794d35b7f955bbb
Kewin Brandsma <kewin@frontend-freelance.com>
2021-08-09 18:51:15 +0200

    cleanup

---
+++ src/engine/animation.cpp
@@ -0,0 +1,62 @@
+#include "./animation.h"
+
+Bone *Animation::FindBone(const std::string &name)
+{
+    auto iter = std::find_if(m_Bones.begin(), m_Bones.end(),
+                             [&](const Bone &Bone)
+                             {
+                                 return Bone.GetBoneName() == name;
+                             });
+    if (iter == m_Bones.end())
+        return nullptr;
+    else
+        return &(*iter);
+}
+
+float Animation::GetTicksPerSecond() { return m_TicksPerSecond; }
+float Animation::GetDuration() { return m_Duration; }
+const AssimpNodeData &Animation::GetRootNode() { return m_RootNode; }
+const std::map<std::string, BoneInfo> &Animation::GetBoneIDMap()
+{
+    return m_BoneInfoMap;
+}
+
+void Animation::SetupBones(const aiAnimation *animation, Model &model)
+{
+    int size = animation->mNumChannels;
+
+    auto &boneInfoMap = model.GetOffsetMatMap();
+    int &boneCount = model.GetBoneCount();
+
+    for (int i = 0; i < size; i++)
+    {
+        auto channel = animation->mChannels[i];
+        std::string boneName = channel->mNodeName.data;
+
+        if (boneInfoMap.find(boneName) == boneInfoMap.end())
+        {
+            boneInfoMap[boneName].id = boneCount;
+            boneCount++;
+        }
+        m_Bones.push_back(Bone(channel->mNodeName.data,
+                               boneInfoMap[channel->mNodeName.data].id, channel));
+    }
+
+    m_BoneInfoMap = boneInfoMap;
+}
+
+void Animation::ReadHeirarchyData(AssimpNodeData &dest, const aiNode *src)
+{
+    assert(src);
+
+    dest.name = src->mName.data;
+    dest.transformation = AssimpGLMHelpers::ConvertMatrixToGLMFormat(src->mTransformation);
+    dest.childrenCount = src->mNumChildren;
+
+    for (int i = 0; i < src->mNumChildren; i++)
+    {
+        AssimpNodeData newData;
+        ReadHeirarchyData(newData, src->mChildren[i]);
+        dest.children.push_back(newData);
+    }
+}
